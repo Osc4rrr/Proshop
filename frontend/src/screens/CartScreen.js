d6089ccf248f9dce57ae5878667d1235c0282efe
oscdiazl <os.diazlobos@gmail.com>
2020-10-09 16:52:36 -0300

    font end authentication and profile done

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -30,7 +30,7 @@ const CartScreen = ({match, location, history}) => {
 
 
     const checkOutHandler = () => {
-        history.push('/login?redirect+shipping')
+        history.push('/login?redirect=shipping')
     }
 
 
