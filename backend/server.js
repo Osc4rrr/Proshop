ebc44705d9f055d982db8ed29ef576cdd79d715f
oscar.diaz <os.diazl@alumnos.duoc.cl>
2020-12-22 00:37:21 -0300

    order finished, reviews and product search

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,16 @@ import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 
+import morgan from 'morgan'; 
+
 dotenv.config(); 
 
 connectDB(); 
 
 const app = express(); 
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'));
+}
 app.use(express.json()); 
 
 app.get('/', (req,res) => {
